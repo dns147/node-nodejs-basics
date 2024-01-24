058d4f9238158c88e9cd3f9c30a5f2071a4d5647
dns147 <rarden@ya.ru>
2024-01-24 23:34:00 +0300

    feat: add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { existsSync } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files/fresh.txt');
+
 const create = async () => {
-    // Write your code here 
-};
+  try {
+    if (existsSync(filePath)) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.appendFile(filePath, 'I am fresh and young');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 await create();
