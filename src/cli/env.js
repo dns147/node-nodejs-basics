b22005f50e37f909bdee90890c8563083eafc61b
dns147 <rarden@ya.ru>
2024-01-26 23:12:18 +0300

    feat: add solution env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envsObject = process.env;
+
+  for (let key in envsObject) {
+    if (key.includes('RSS_')) {
+      console.log(`${key}=${envsObject[key]}`);
+    }
+  }
 };
 
 parseEnv();
