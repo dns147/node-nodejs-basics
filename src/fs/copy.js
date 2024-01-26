a32e355261a5d2976b9fa3216a731ba406d0db3b
dns147 <rarden@ya.ru>
2024-01-26 21:23:15 +0300

    feat: add copy file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,44 @@
+import * as fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { copyFile, existsSync, mkdir, readdir } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dirPath = path.join(__dirname, 'files');
+const dirCopyPath = path.join(__dirname, 'files_copy');
+
+const eventError = (err) => {
+  if (err) {
+    throw err;
+  }
+};
+
+const copyFiles = (file) => {
+  const fileSrcPath = path.join(dirPath, file);
+  const fileDestPath = path.join(dirCopyPath, file);
+
+  copyFile(fileSrcPath, fileDestPath, eventError);
+};
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(dirPath) || existsSync(dirCopyPath)) {
+      throw new Error('FS operation failed');
+    } else {
+      mkdir(dirCopyPath, eventError);
+      readdir(dirPath, (err, files) => {
+        if (err) {
+          throw err;
+        }
+                
+        files.forEach(copyFiles);
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await copy();
