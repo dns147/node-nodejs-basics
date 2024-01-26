79657b7c2fcda63cc5506d543dc3d859af3b6941
dns147 <rarden@ya.ru>
2024-01-26 22:29:07 +0300

    feat: add read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import * as fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createReadStream, existsSync } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(filePath)) {
+      throw new Error('FS operation failed');
+    } else {
+      const stream = createReadStream(filePath);
+
+      stream.on('data', (data) => {
+        const dataFile = Buffer.from(data);
+        process.stdout.write(dataFile); // 1-ый способ вывода
+        //console.log(dataFile.toString()); // 2-ой способ вывода
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await read();
