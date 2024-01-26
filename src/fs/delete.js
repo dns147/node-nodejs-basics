9084bdd92c68fee2452ec8888211978fd5626f58
dns147 <rarden@ya.ru>
2024-01-26 22:03:46 +0300

    feat: add delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,27 @@
+import * as fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { existsSync } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const deleteFilename = path.join(__dirname, 'files/fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(deleteFilename)) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.unlink(deleteFilename, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await remove();
