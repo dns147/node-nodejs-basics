a237c4986c59d0d32d12f80cade406917bd0d8b1
dns147 <rarden@ya.ru>
2024-01-26 23:46:28 +0300

    feat: add solution args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+
+  for (let i = 0; i < args.length; i += 2) {
+    console.log(`${args[i]} is ${args[i + 1]}`);
+  }
 };
 
 parseArgs();
