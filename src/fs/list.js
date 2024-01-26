8e3911b171bd1acade483730340b6453f91c9137
dns147 <rarden@ya.ru>
2024-01-26 22:16:00 +0300

    feat: add list files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,29 @@
+import * as fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { existsSync, readdir } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dirPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(dirPath)) {
+      throw new Error('FS operation failed');
+    } else {
+      readdir(dirPath, (err, files) => {
+        if (err) {
+          throw err;
+        }
+                
+        console.log(files);
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await list();
