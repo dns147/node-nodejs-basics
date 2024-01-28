137391fc71b958d10c3e78e0c05da49460710268
dns147 <rarden@ya.ru>
2024-01-28 12:09:26 +0300

    feat: add solution hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256').setEncoding('hex');
+  //const hash = createHash('sha256').update(content).digest('hex'); 
+
+  createReadStream(filePath)
+    .pipe(hash)
+    .on('finish', function() {
+      const fileHash = hash.read();
+      const stream = new WritableStream({
+        write(chunk) {
+          console.log(chunk);
+        },
+      });
+
+      stream.getWriter().write(fileHash);
+    });
 };
 
 await calculateHash();
