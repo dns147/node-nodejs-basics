d4ab9f386c8903ef01e190bca6e751a1af76fc0a
dns147 <rarden@ya.ru>
2024-01-28 14:03:24 +0300

    feat: add solution streams write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createWriteStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(filePath);
+
+  process.stdin.pipe(stream);
+  process.stdin.resume();
 };
 
 await write();
