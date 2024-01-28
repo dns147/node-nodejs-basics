18047c12ad41720739cf84652a89869e7a8ed9b5
dns147 <rarden@ya.ru>
2024-01-28 15:46:07 +0300

    feat: add solution streams transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform (data, encoding, callback) {
+      const reversedData = data.toString().split('').reverse().join('');
+      this.push(reversedData);
+      callback();
+    }
+  });
+
+  process.stdin.pipe(reverseStream).on('data', (data) => {
+    const dataFile = Buffer.from(data);
+    process.stdout.write(dataFile);
+  });
+
+  process.stdin.resume();
 };
 
 await transform();
