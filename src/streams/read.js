c26dafe39a366b3783d88b5a40938e6a33b04212
dns147 <rarden@ya.ru>
2024-01-28 13:07:41 +0300

    feat: add solution streams read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createReadStream } from 'node:fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (data) => {
+    const dataFile = Buffer.from(data);
+    process.stdout.write(dataFile);
+  });
 };
 
 await read();
