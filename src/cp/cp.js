dc9ae908c92a1b0373f54fc9f6623b12d68e5c80
dns147 <rarden@ya.ru>
2024-01-30 00:31:41 +0300

    feat: add solution child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from 'child_process';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, './files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = fork(filePath, args);
+
+  childProcess.on('message', (msg) => {
+    console.log('PARENT got message:', msg);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 3, 5]);
